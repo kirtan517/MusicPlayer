ddfd49e3cc26d9d9e9ae4e41216b2ce014e63d06
kirtan517 <kananikirtan73@gmail.com>
2023-02-23 16:57:59 -0500

    First page Done

---
+++ music_player_ui/src/components/Song/Table.tsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { IconButton } from "@material-ui/core";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  //   "&:nth-of-type(odd)": {
+  //     backgroundColor: theme.palette.action.hover,
+  //   },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+  "&:hover": {
+    background: "rgba(0, 0, 0, 0.28)",
+    boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+    backdropFilter: "blur(10.7px)",
+    WebkitBackdropFilter: "blur(10.7px)",
+    border: "1px solid rgba(99, 93, 93, 1)",
+  },
+
+  background: "#000000",
+}));
+
+function createData(song: string, likes: number, date: string) {
+  return { song, likes, date };
+}
+
+const styles = {
+  color: "white",
+};
+
+const rows = [
+  createData("Frozen yoghurt", 159, "2023"),
+  createData("Ice cream sandwich", 237, "2022"),
+  createData("Eclair", 262, "2021"),
+  createData("Cupcake", 305, "2020"),
+  createData("Gingerbread", 356, "2021"),
+  createData("Frozen yoghurt", 159, "2023"),
+  createData("Ice cream sandwich", 237, "2022"),
+  createData("Eclair", 262, "2021"),
+  createData("Cupcake", 305, "2020"),
+  createData("Gingerbread", 356, "2021"),
+];
+
+export default function CustomizedTables() {
+  return (
+    <TableContainer
+      component={Paper}
+      style={{
+        margin: "10px 0px",
+        padding: "10px 20px",
+        background: "#000000",
+      }}
+    >
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <colgroup>
+          <col style={{ width: "40%" }} />
+          <col style={{ width: "5%" }} />
+          <col style={{ width: "5%" }} />
+          <col style={{ width: "20%" }} />
+          <col style={{ width: "20%" }} />
+        </colgroup>
+        {/* <TableHead>
+          <TableRow>
+            <StyledTableCell>Song</StyledTableCell>
+            <StyledTableCell align="right">Likes</StyledTableCell>
+            <StyledTableCell align="right">Date</StyledTableCell>
+          </TableRow>
+        </TableHead> */}
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.song}>
+              <StyledTableCell component="th" scope="row">
+                {row.song}
+              </StyledTableCell>
+              <StyledTableCell>
+                <IconButton
+                  aria-label="wishList"
+                  style={styles}
+                  color="inherit"
+                >
+                  <FavoriteBorderOutlinedIcon />
+                </IconButton>
+              </StyledTableCell>
+              <StyledTableCell>
+                <IconButton aria-label="like" style={styles} color="inherit">
+                  <ThumbUpOutlinedIcon />
+                </IconButton>
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.likes}</StyledTableCell>
+              <StyledTableCell align="right">{row.date}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- music_player_ui/src/components/Song/songComponent.tsx
+++ music_player_ui/src/components/Song/songComponent.tsx
@@ -1,299 +1,35 @@
 import {
   AntDesignOutlined,
-  DownloadOutlined,
   HeartFilled,
   HeartOutlined,
   LikeFilled,
-  LikeOutlined,
   PlayCircleFilled,
-  PlayCircleOutlined,
 } from "@ant-design/icons";
-import {
-  Avatar,
-  Button,
-  Col,
-  ConfigProvider,
-  Image,
-  Row,
-  Skeleton,
-  Space,
-  Tag,
-} from "antd";
+import { Avatar, Button, Image, Space } from "antd";
 import { SizeType } from "antd/es/config-provider/SizeContext";
-import Table, { ColumnsType } from "antd/es/table";
 import { useState } from "react";
-import {
-  notificationProvider,
-  Layout,
-  ErrorComponent,
-  theme,
-} from "@pankod/refine-antd";
-import { Container, Typography, Grid, Paper } from "@material-ui/core";
+import { Container, Typography, Grid } from "@material-ui/core";
 import TS from "../../images/TS.png";
 import "./songComponent.css";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { TableVirtuoso, TableComponents } from 'react-virtuoso';
+import CustomizedTables from "./Table";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import PlayCircleOutlinedIcon from '@mui/icons-material/PlayCircleOutlined';
 
-
-interface Data {
-  calories: number;
-  carbs: number;
-  dessert: string;
-  fat: number;
-  id: number;
-  protein: number;
-}
-
-interface ColumnData {
-  dataKey: keyof Data;
-  label: string;
-  numeric?: boolean;
-  width: number;
-}
-
-type Sample = [string, number, number, number, number];
-
-const sample: readonly Sample[] = [
-  ["Frozen yoghurt", 159, 6.0, 24, 4.0],
-  ["Ice cream sandwich", 237, 9.0, 37, 4.3],
-  ["Eclair", 262, 16.0, 24, 6.0],
-  ["Cupcake", 305, 3.7, 67, 4.3],
-  ["Gingerbread", 356, 16.0, 49, 3.9],
-];
-
-function createData(
-  id: number,
-  dessert: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-): Data {
-  return { id, dessert, calories, fat, carbs, protein };
-}
-
-const columns: ColumnData[] = [
-  {
-    width: 200,
-    label: "Dessert",
-    dataKey: "dessert",
-  },
-  {
-    width: 120,
-    label: "Calories\u00A0(g)",
-    dataKey: "calories",
-    numeric: true,
-  },
-  {
-    width: 120,
-    label: "Fat\u00A0(g)",
-    dataKey: "fat",
-    numeric: true,
-  },
-  {
-    width: 120,
-    label: "Carbs\u00A0(g)",
-    dataKey: "carbs",
-    numeric: true,
-  },
-  {
-    width: 120,
-    label: "Protein\u00A0(g)",
-    dataKey: "protein",
-    numeric: true,
-  },
-];
-
-const rows: Data[] = Array.from({ length: 200 }, (_, index) => {
-  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-  return createData(index, ...randomSelection);
-});
-
-const VirtuosoTableComponents: TableComponents<Data> = {
-  Scroller: React.forwardRef<HTMLDivElement>((props, ref) => (
-    <TableContainer component={Paper} {...props} ref={ref} />
-  )),
-  Table: (props) => (
-    <Table
-      {...props}
-      sx={{ borderCollapse: "separate", tableLayout: "fixed" }}
-    />
-  ),
-  TableHead,
-  TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
-  TableBody: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
-    <TableBody {...props} ref={ref} />
-  )),
-};
-
-function fixedHeaderContent() {
-  return (
-    <TableRow>
-      {columns.map((column) => (
-        <TableCell
-          key={column.dataKey}
-          variant="head"
-          align={column.numeric || false ? "right" : "left"}
-          style={{ width: column.width }}
-          sx={{
-            backgroundColor: "background.paper",
-          }}
-        >
-          {column.label}
-        </TableCell>
-      ))}
-    </TableRow>
-  );
-}
-
-function rowContent(_index: number, row: Data) {
-  return (
-    <React.Fragment>
-      {columns.map((column) => (
-        <TableCell
-          key={column.dataKey}
-          align={column.numeric || false ? "right" : "left"}
-        >
-          {row[column.dataKey]}
-        </TableCell>
-      ))}
-    </React.Fragment>
-  );
-}
-
-interface DataType {
-  key: string;
-  writer: string;
-  song: string;
-  likes: number;
-}
 
 const SongComponent = () => {
   const [size, setSize] = useState<SizeType>("large");
 
-  const columns: ColumnsType<DataType> = [
-    {
-      title: "Artist",
-      dataIndex: "writer",
-      key: "writer",
-      render: (text) => <a>{text}</a>,
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
     },
-    {
-      title: "Song",
-      dataIndex: "song",
-      key: "song",
-    },
-    {
-      title: "Likes",
-      dataIndex: "likes",
-      key: "likes",
-    },
+  });
 
-    {
-      title: "Action",
-      key: "action",
-      render: (_, record) => (
-        <Space size="middle">
-          <a>Invite {record.writer}</a>
-          <a>Delete</a>
-        </Space>
-      ),
-    },
-  ];
-
-  const data: DataType[] = [
-    {
-      key: "1",
-      writer: "John Brown",
-      song: "New York No. 1 Lake Park",
-      likes: 32,
-    },
-    {
-      key: "2",
-      writer: "Jim Green",
-      song: "London No. 1 Lake Park",
-      likes: 54,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-    {
-      key: "3",
-      writer: "Joe Black",
-      song: "Sydney No. 1 Lake Park",
-      likes: 98,
-    },
-  ];
+  const inputGlobalStyles = <GlobalStyles styles={{ padding: "20px 20px", background : "red important!" }} />;
 
   const SongDescription = (
     <Container
@@ -302,8 +38,6 @@ const SongComponent = () => {
       style={{
         background: "rgba(0, 0, 0, 0.0)",
         boxShadow: "0 0px 30px rgba(0, 0, 0, 0.1)",
-        // backdropFilter: "blur(7.6px)",
-        // WebkitBackdropFilter: "blur(11.6px)",
         borderTop: "1px solid rgba(0, 0, 0, 0.39)",
         padding: "0",
       }}
@@ -320,7 +54,7 @@ const SongComponent = () => {
               type="primary"
               shape="circle"
               block={true}
-              icon={<PlayCircleFilled style={{ fontSize: "inherit" }} />}
+              icon={<PlayCircleOutlinedIcon style={{ fontSize: "inherit" }} />}
               size={size}
               ghost={true}
               style={{
@@ -336,7 +70,7 @@ const SongComponent = () => {
             <Button
               type="primary"
               shape="circle"
-              icon={<HeartFilled />}
+              icon={<FavoriteBorderOutlinedIcon style={{ fontSize: "inherit" }} />}
               size={size}
               ghost={true}
               style={{
@@ -359,7 +93,7 @@ const SongComponent = () => {
             <Button
               type="primary"
               shape="round"
-              icon={<LikeFilled style={{ fontSize: "inherit" }} />}
+              icon={<ThumbUpOutlinedIcon style={{ fontSize: "inherit" }} />}
               size={size}
               ghost={true}
               style={{
@@ -405,11 +139,7 @@ const SongComponent = () => {
   );
 
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: theme.darkAlgorithm,
-      }}
-    >
+    <>
       <Container
         component="main"
         style={{
@@ -505,16 +235,12 @@ const SongComponent = () => {
           </Typography>
         </div>
 
-        <Paper style={{ height: 400, width: "100%" }}>
-          <TableVirtuoso
-            data={rows}
-            components={VirtuosoTableComponents}
-            fixedHeaderContent={fixedHeaderContent}
-            itemContent={rowContent}
-          />
-        </Paper>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <CustomizedTables />
+        </ThemeProvider>
       </Container>
-    </ConfigProvider>
+    </>
   );
 };
 
